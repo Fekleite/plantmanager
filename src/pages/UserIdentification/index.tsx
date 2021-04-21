95757824b811c370f254674a993e9b2914757a60
Fekleite <fernandacarolineleite360@gmail.com>
2021-04-20 21:04:31 -0300

    feat: create user identification page

---
+++ src/pages/UserIdentification/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View, KeyboardAvoidingView, Platform } from 'react-native';
+
+import styles from './styles';
+
+import { SafeAreaViewAndroid } from '../../components/SafeAreaViewAndroid';
+import { Button } from '../../components/Button';
+
+export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  function handleInputBlur() {
+    setIsFocused(false)
+    setIsFilled(!!name)
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true)
+  }
+
+  function handleInputChange(value: string) {
+    setIsFilled(!!value)
+    setName(value)
+  }
+
+  return (
+    <SafeAreaViewAndroid style={styles.container} >
+      <KeyboardAvoidingView 
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.emoji}>
+                { isFilled ? '😁' : '😀' } 
+              </Text>
+              <Text style={styles.title}>
+                Como podemos {'\n'}
+                chamar você?
+              </Text>
+            </View>
+
+            <TextInput 
+              style={[
+                styles.input,
+                (isFocused || isFilled) && styles.inputFocused
+              ]}
+              placeholder='Digite um nome'
+              placeholderTextColor='#CFCFCF'
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChangeText={handleInputChange}
+            />
+
+            <View style={styles.footer}>
+              <Button text='Confirmar' />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaViewAndroid>
+  )
+}
